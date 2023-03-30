297922d808ed530329bbfd20b7ac2c7cbc0d4f15
Guido Espinoza <guidoespinozac@gmail.com>
2023-03-30 18:21:59 -0300

    corrección efecto profileImage

--- assets/js/profileImage.js
+++ assets/js/profileImage.js
@@ -1,35 +1,37 @@
 //* Animación Profile Image
 
 export function profileImage() {
-    const profileImage = document.getElementById('profileImage')
-    const width = profileImage.clientWidth
-    const height = profileImage.clientHeight
-
-    profileImage.addEventListener('mousemove', (evt) => {
+    if (window.innerWidth > 768) {
+      const profileImage = document.getElementById('profileImage')
+      const width = profileImage.clientWidth
+      const height = profileImage.clientHeight
+  
+      profileImage.addEventListener('mousemove', (evt) => {
         const { layerX, layerY } = evt
-
+  
         const yRotation = (
-            (layerX - width / 2) / width
+          (layerX - width / 2) / width
         ) * 8
-
+  
         const xRotation = (
-            (layerY - height / 2) / height
+          (layerY - height / 2) / height
         ) * 8
-
+  
         const string = `
-        perspective(1200px)
-        scale(1)
-        rotateX(${xRotation}deg)
-        rotateY(${yRotation}deg)`
-
+          perspective(1200px)
+          scale(1)
+          rotateX(${xRotation}deg)
+          rotateY(${yRotation}deg)`
+  
         profileImage.style.transform = string
-    })
-
-    profileImage.addEventListener('mouseout', () => {
+      })
+  
+      profileImage.addEventListener('mouseout', () => {
         profileImage.style.transform = `
-        perspective(1200px)
-        scale(1)
-        rotateX(0)
-        rotateY(0)`
-    })
-}
+          perspective(1200px)
+          scale(1)
+          rotateX(0)
+          rotateY(0)`
+      })
+    }
+  }
